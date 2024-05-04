5bc08061bfcec4e80235425d7cf846d201a1e667
Powerlifter2137 <keygle@protonmail.com>
2024-05-04 09:57:11 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int result = MathUtils.add(12, 6);
+        System.out.println("Results: " + result);
+
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("this is email message");
+        emailMessenger.sendMessage("Results: " + result);
     }
 }
